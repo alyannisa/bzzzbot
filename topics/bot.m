0c2deef549935963639fc7dc03564c4791451488
Nurul Alyannisa <55867525+alyannisa@users.noreply.github.com>
2020-11-22 21:27:26 +0700

    Create bot.m

---
+++ topics/bot.m
@@ -0,0 +1,59 @@
+
+Q: Cooking
+A: I will give you some recipes based on fruits! Please type (strawberry/banana/blueberry)! &#127827 &#127820 &#127815
+K: $back cook
+--
+
+Q: Game
+A: Type any fruit to guess my favorite fruit &#129373 &#127822 &#127827!
+K: $back game
+--
+
+Q: Fruit
+A: Great! &#127775 What should we do? <br> ${"Game" button} ${"Cooking" button}
+K: $back fruit
+--
+
+Q: Korean
+A: Great! &#127775 what genre do you like? Romance &#128525? Action &#128526?
+K: $back kdrama
+--
+
+Q: Indonesian
+A: Great! &#127775 what genre do you like? Adventure &#10024? Romance &#128152?
+K: $back indo
+--
+
+Q: American
+A: Great! &#127775 what genre do you like? Romance &#128535 &#128150? Comedy &#128518?
+K: $back american
+--
+
+Q: Movie
+A: Great! Choose what you like! &#127916 &#127942 &#128150 <br> ${"Korean" button} <br> ${"Indonesian" button} <br> ${"American" button}
+K: $back Movie 
+\ ; $back american ; $back indo ; $back kdrama
+--
+
+Q: Get to know each other
+A: Great! Try to ask my name first &#128523
+K: $back greeting
+--
+
+Q: Quit
+A: Thank you for using this bot! &#129336 &#128150 https://www.memesmonkey.com/images/memesmonkey/c0/c0e9fb02e92270d76ee8090084f7f5ef.jpeg <br> See you again!
+--
+Q: leave
+A: You're leaving too fast! &#128557&#128555 https://i.pinimg.com/originals/fe/0b/ca/fe0bcaf8ebbd43edba2642305026c191.jpg <br> Bye!
+--
+Q: $_
+A: So, What do you want to talk about? &#128173 <br> <br> Psst! Just Click it! &#128317 <br> ${"Get to know each other" button} <br> ${"Movie" button} ${"Fruit" button} <br> ${"Quit" button}
+K: $back fruit ; $back Movie ; $back greeting
+\ ; $back american ; $back indo ; $back kdrama
+--
+
+
+
+
+
+
