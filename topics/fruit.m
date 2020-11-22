5a0bc15e2f73fca2d4389cf23047e001731cc5c7
Nurul Alyannisa <55867525+alyannisa@users.noreply.github.com>
2020-11-22 21:24:26 +0700

    Create fruit.m

---
+++ topics/fruit.m
@@ -0,0 +1,35 @@
+@: fruit loves fruit2
+fruit: apple banana grape melon lemon orange mango cherry kiwi strawberry pear
+fruit2: durian peach starfruit
+loves: like love LOVE am_into
+
+room: game
+
+Q: $x.@fruit or|to $y.@fruit
+A: I like them both ; I like both $x and $y
+--
+
+Q: $x.@fruit or|to $_
+Q: $loves $_ or|to $x.@fruit
+A: I prefer $x
+--
+
+Q: $fruit|$fruit+s
+A: Yes I do ; I love $fruit ; https://p7.hiclipart.com/preview/3/746/347/5bbc0b7f02480.jpg ; Yes I love it! How about you?
+--
+
+Q: $fruit2|$fruit2+s
+A: NAH http://www.quickmeme.com/img/70/70269c10c1365020e03134ef05c1f7384704cbab2681ffadd54593c2222819a7.jpg ; UHM NOO https://www.mememaker.net/api/bucket?path=static/img/memes/full/2018/Apr/25/12/no-84.png
+
+Q: I $loves too
+Q: Same with you
+Q: Me too
+A: Great! https://memegenerator.net/img/instances/54794287.jpg ; https://www.mememaker.net/api/bucket?path=static/img/memes/full/2016/Jan/18/2/thats-great72.jpg
+--
+
+\ Q: $_
+\ A: I haven't try it ; https://www.viraluproar.com/wp-content/uploads/2016/08/i-dont-know-meme.jpg
+\ \ L: $_
+\ --
+
+end-room
